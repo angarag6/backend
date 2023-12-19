6c94c47ae8a9a8f049c6f45e1b4088849f945673
Ангараг Батдэлгэр <23LP7433@G303-CC0139.local>
2023-12-19 10:57:01 +0800

    dgfa

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
+import express from "express";
+
 const express = require("express");
-const port = 8000;
+const port = 8001;
 const app = express();
+const users = [ 
+  {
+  id: 1,
+  name: "an",
+},
+{
+  id: 2,
+  name: "nak",
+},
+{
+  id: 3,
+  name: "ko",
+},
+]
 app.get("/", (req, res) => {
-  res.send("sad");
+  res.send({success:true, users: users}).end();
+});
+app.post("/", (req, res) => {
+  res.send({success:true, users: users}).end();
+});
+app.put("/", (req, res) => {
+  res.send({success:true, method: 'put'}).end();
 });
 app.listen(port, () => {
-  console.log("hi");
+  console.log("hh");
 });
